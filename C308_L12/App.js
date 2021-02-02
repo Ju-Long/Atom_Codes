b19dd37a589c00143d434effa834ee936ae2ef3b
Ju Long <19013345@myrp.edu.sg>
2021-02-02 11:59:20 +0800

    created L12

---
+++ C308_L12/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: () => React$Node = () => {
+  return (
+    <></>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ C308_L12/src/ValistApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const ValistApp: () => React$Node = () => {
+    return (
+        <></>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ValistApp;
